12d10db7ab3d7b7154c9adf0c4180f0f88a451f7
Gamaliel19 <yagalinaf@gmail.com>
2026-03-23 14:38:53 +0100

    Deuxième commit

--- exercice1.py
+++ exercice1.py
@@ -1,5 +1,10 @@
-A = int(input("Entrez un nombre entier : "))
+""" A = int(input("Entrez un nombre entier : "))
 B = int(input("Entrez un autre nombre entier : "))
 
 S = A + B
-print("La somme de", A, "et", B, "est :", S)
+print("La somme de", A, "et", B, "est :", S)"""
+
+
+temp = float(input("Entrez la température: "))
+carreTemp = temp * temp
+print("Le carré de la température est :", carreTemp, "°C²")
